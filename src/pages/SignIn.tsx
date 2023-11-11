02dfe5294736f2d06a3e82881dcf3a475ddddfd3
José Miguel Oroño <joseomaker@gmail.com>
2023-11-11 10:35:42 -0500

    Add Sign In interfaces and logic

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -6,6 +6,45 @@ import {
     Button
 } from '@chakra-ui/react';
 
+import axios, { AxiosResponse } from 'axios';
+
+interface signInRequestBody {
+    email: string;
+    password: string;
+}
+
+interface signInRequestOption {
+    method: string;
+    url: string;
+    headers: any;
+    data: signInRequestBody;
+}
+
+
+const signInUser = async (userData: signInRequestBody): Promise<void> => {
+    const api_url = import.meta.env.VITE_API_AUTH_URL
+    
+    const options:signInRequestOption = {
+        method: 'POST',
+        url: api_url + '/login/',
+        headers: {'Content-Type': 'application/json'},
+        data: userData
+    };
+    
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error('(Axios) Login failed:', error.message);
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          console.error('Response data:', error.response.data);
+          console.error('Response status:', error.response.status);
+          console.error('Response headers:', error.response.headers);
+        }
+    });
+}
+
+
 export const SignIn = () => {
     return (
         <form action="">
