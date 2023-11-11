a068e0b6631c050f69b5c3e3ecca1f9b4d61f8e9
José Miguel Oroño <joseomaker@gmail.com>
2023-11-11 09:03:12 -0500

    Add boilerplate for the new libraries

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+})
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
