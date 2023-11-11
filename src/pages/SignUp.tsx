e9b80ddab4dba28734f6a38d114817e857bcc4be
José Miguel Oroño <joseomaker@gmail.com>
2023-11-11 10:26:59 -0500

    Import Axios and create registerUser function

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -6,6 +6,59 @@ import {
     Button
 } from '@chakra-ui/react';
 
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
+interface SignUpRequestBody {
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+}
+
+const api_url = import.meta.env.VITE_API_AUTH_URL
+
+const registerUser = async (userData: SignUpRequestBody): Promise<void> => {
+    try {
+      const response: AxiosResponse = await axios.post(api_url+'/register/', userData, {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      });
+  
+      // Handle successful registration
+      console.log('Registration successful!', response.data);
+    } catch (error: any) {
+      if (axios.isAxiosError(error)) {
+        // Axios error (e.g., network error, response status not in 2xx)
+        console.error('(Axios) Registration failed:', error.message);
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          console.error('Response data:', error.response.data);
+          console.error('Response status:', error.response.status);
+          console.error('Response headers:', error.response.headers);
+        }
+      } else {
+        // Non-Axios error
+        console.error('An unexpected error occurred:', error);
+      }
+    }
+  };
+
+
+/*
+Ejemplo de uso de la funcionde Registro
+
+const userData: SignUpRequestBody = {
+  first_name: 'gerardo',
+  last_name: 'torres',
+  email: 'gtorres@binwus.com',
+  password: '123456',
+};
+Para usarlo...
+registerUser(userData);
+*/
+
 export const SignUp = () => {
     return (
         <form action="">
