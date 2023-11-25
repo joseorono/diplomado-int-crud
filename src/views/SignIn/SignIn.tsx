70db230b869897f94a4637b99f58ff279256d68a
Nathalie Zambrano <nathalievzm@gmail.com>
2023-11-25 12:22:18 -0600

    chore: configure prettier

--- src/views/SignIn/SignIn.tsx
+++ src/views/SignIn/SignIn.tsx
@@ -1,84 +1,75 @@
-import "./SignIn.css";
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  FormHelperText,
-  Button,
-  Flex,
-} from "@chakra-ui/react";
-import { PiCodeBold } from "react-icons/pi";
-import { signInUser } from "../../api";
-import { useAuthStore } from "../../store";
-import { useEffect, useState } from "react";
-import axios, { AxiosResponse } from "axios";
-import { toast } from "react-toastify";
-import { SingUpRequestResponse } from "../../interfaces";
-import { Link } from "wouter";
+import './SignIn.css'
+import { FormControl, FormLabel, Input, FormHelperText, Button, Flex } from '@chakra-ui/react'
+import { PiCodeBold } from 'react-icons/pi'
+import { signInUser } from '../../api'
+import { useAuthStore } from '../../store'
+import { useEffect, useState } from 'react'
+import axios, { AxiosResponse } from 'axios'
+import { toast } from 'react-toastify'
+import { SingUpRequestResponse } from '../../interfaces'
+import { Link } from 'wouter'
 
 export const SignIn = () => {
-  const { grantAuthentication } = useAuthStore();
+  const { grantAuthentication } = useAuthStore()
   const [authForm, setAuthForm] = useState({
-    email: "",
-    password: "",
-  });
-  const [disable, setDisableButton] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(false);
+    email: '',
+    password: ''
+  })
+  const [disable, setDisableButton] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
 
-  const { email, password } = authForm;
+  const { email, password } = authForm
 
   useEffect(() => {
-    setDisableButton(!email || !password);
-  }, [email, password]);
+    setDisableButton(!email || !password)
+  }, [email, password])
 
   // manage changer events
-  const handleChange = (event) => {
+  const handleChange = event => {
     setAuthForm({
       ...authForm,
-      [(event.target as HTMLInputElement).name]: (
-        event.target as HTMLInputElement
-      ).value,
-    });
-  };
+      [(event.target as HTMLInputElement).name]: (event.target as HTMLInputElement).value
+    })
+  }
 
   // manage user click register
   const handleOnClickButton = async () => {
     try {
       // validate form is completed
-      if (!email || !password) return;
+      if (!email || !password) return
 
       // active loading
-      setLoading(true);
+      setLoading(true)
 
-      const response: AxiosResponse = await signInUser(authForm);
+      const response: AxiosResponse = await signInUser(authForm)
 
       if (response.status !== 200) {
-        toast.error(response.data);
-        return;
+        toast.error(response.data)
+        return
       }
 
-      toast.success("Login successfully");
+      toast.success('Login successfully')
 
       // save user authentication
-      grantAuthentication((response.data as SingUpRequestResponse).token);
+      grantAuthentication((response.data as SingUpRequestResponse).token)
     } catch (error) {
-      console.error(error);
+      console.error(error)
       if (axios.isAxiosError(error)) {
-        toast.error(JSON.stringify(error?.response?.data));
+        toast.error(JSON.stringify(error?.response?.data))
       } else {
-        toast.error("Error");
+        toast.error('Error')
       }
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
-  };
+  }
 
   return (
-    <Flex w={"100%"} h={"100%"} justifyContent={"center"} alignItems={"center"}>
+    <Flex w={'100%'} h={'100%'} justifyContent={'center'} alignItems={'center'}>
       <form action='' id='sign-in'>
         <header>
           <section>
-            <PiCodeBold size={"1.5rem"} />
+            <PiCodeBold size={'1.5rem'} />
             <h1>DEVS</h1>
           </section>
           <section>
@@ -95,7 +86,7 @@ export const SignIn = () => {
             <Input
               name='email'
               type='email'
-              borderRadius={"1.5rem"}
+              borderRadius={'1.5rem'}
               placeholder='you@example.com'
               value={email}
             />
@@ -107,7 +98,7 @@ export const SignIn = () => {
             <Input
               name='password'
               type='password'
-              borderRadius={"1.5rem"}
+              borderRadius={'1.5rem'}
               placeholder='Must be 8 characters long'
               value={password}
             />
@@ -120,14 +111,13 @@ export const SignIn = () => {
             disabled={disable || loading}
             onClick={handleOnClickButton}
             colorScheme='blue'
-            borderRadius={"1.5rem"}
-            lineHeight={"inherit"}
-            size={"lg"}
-          >
+            borderRadius={'1.5rem'}
+            lineHeight={'inherit'}
+            size={'lg'}>
             Sign in
           </Button>
         </footer>
       </form>
     </Flex>
-  );
-};
+  )
+}
